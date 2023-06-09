07bf35bdee0cd5bc7d40d019329024694190db2c
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-09 21:20:12 +0600

    auth provider setup 50%

--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,46 @@
+import { createContext, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    
+
+    const authInfo = {
+        user,
+        loading,
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
