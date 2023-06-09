cba78486de0a752a2d478cb0d73cf42c186ddefa
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-10 01:42:52 +0600

    disable btn added on classes card

--- src/pages/ClassesPage/ApprovedClassCard.jsx
+++ src/pages/ClassesPage/ApprovedClassCard.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const ApprovedClassCard = ({ApprovedClass}) => {
 
     const { image, name, instructorName, availableSeats, students, price, _id, } = ApprovedClass;
-    const [disable, setDisable] = useState(false)
+
     
     const setLeft = availableSeats - students;
 
@@ -13,19 +14,18 @@ const ApprovedClassCard = ({ApprovedClass}) => {
     // }
 
     return (
-        <div className={`card w-full glass ${setLeft === 0 ? 'bg-red-600 opacity-80' : ''}`}>
+        <div className={`card w-full glass ${setLeft === 0 ? 'bg-red-700 opacity-80' : ''}`}>
             <figure><img src={image} alt="car!" /></figure>
             <div className="card-body w-full">
-                <h2 className="card-title text-white opacity-90">Learn <span className="text-cyan-400 text-xl">{name}</span> with our instructor</h2>
+                <h2 className="card-title text-white opacity-90">Learn <span className="text-cyan-400 ">{name}</span> with our instructor</h2>
                 <div className="flex w-full items-center justify-between">
-                    <p><span className="font-semibold text-white opacity-80">Instructor:</span> <span className="text-cyan-400 text-xl">{instructorName}</span></p>
-                <p><span className="font-semibold text-white opacity-80">Price:</span> <span className="text-cyan-400 text-xl">${price}</span></p>
+                    <p><span className="font-semibold text-white opacity-80">Instructor:</span> <span className="text-cyan-400 ">{instructorName}</span></p>
+                <p><span className="font-semibold text-xl text-white opacity-80">Price:</span> <span className="text-cyan-400 font-bold text-xl">${price}</span></p>
                 </div>
                 <div className="flex w-full items-center justify-between">
-                <p><span className="font-semibold text-white opacity-80">Available Sets:</span> <span className="text-cyan-400 text-xl">{setLeft}</span></p>
-                    <p><span className="font-semibold text-white opacity-80">Students:</span> <span className="text-cyan-400 text-xl">{students}</span></p>
+                <p><span className="font-semibold text-white opacity-80">Available Sets:</span> <span className="text-cyan-400 ">{setLeft}</span></p>
                 </div>
-                <button disabled={setLeft === 0 ? true : false} className="btn btn-outline btn-info btn-block mt-3">Select Course</button>
+                <button disabled={setLeft === 0 ? true : false} className="btn btn-outline btn-info btn-block mt-3"><Link to={`/classes/${_id}`}>Select Course</Link></button>
             </div>
         </div>
     );
