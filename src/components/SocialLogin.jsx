3ca38d6d244a5b68aedc2e53bbb3496b0d010064
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-12 12:22:21 +0600

    post method added for saving user by google login

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,19 +1,55 @@
 import { useContext } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { AuthContext } from "../Providers/AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
 
     const { googleLogin } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const handleSocialLogIn = () => {
+
         googleLogin()
             .then(res => {
                 const loggedUser = res.user;
-                console.log(loggedUser);
-            }).catch(err => {
-            console.error(err.message)
-        })
+                
+
+                const savedUser = { name: loggedUser.displayName, email: loggedUser.email, role: 'student' }
+
+                fetch(`http://localhost:5000/users`, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(savedUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+                            navigate('/')
+                            const Toast = Swal.mixin({
+                                toast: true,
+                                position: 'top-end',
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                }
+                            })
+                              
+                            Toast.fire({
+                                icon: 'success',
+                                title: 'Signed in successfully'
+                            })
+                        }
+                    }).catch(err => {
+                        console.error(err.message)
+                    })
+            })
     }
 
     return (
