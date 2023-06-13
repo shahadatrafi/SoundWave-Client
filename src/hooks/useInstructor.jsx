dfbdb2af16839204cbbc2d43dcd8da6fafb19a32
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-14 03:21:48 +0600

    verify instructor hook created

---
+++ src/hooks/useInstructor.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+
+const useInstructor = () => {
+    const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
+    const {data: isInstructor, isLoading: isInstructorLoading } = useQuery({
+        queryKey: ['isInstructor', user?.email],
+        queryFn: async() => {
+            const res = await fetch(`http://localhost:5000/users/instructor/${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            });
+            return res.json();
+        }
+    })
+    return [isInstructor, isInstructorLoading]
+};
+
+export default useInstructor;
