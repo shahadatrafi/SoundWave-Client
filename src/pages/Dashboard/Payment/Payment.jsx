963e7930f7a7f73628ed16d0a1c1ecd009da3f16
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-14 20:37:28 +0600

    checkout form created

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,20 @@
+import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../components/SectionTitle";
+import CheckoutForm from "../../../components/CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
 
 
 const Payment = () => {
+
+    const stripePromise = loadStripe('');
+
+
     return (
         <div>
             <SectionTitle heading={'Payment'} subheading={'Enroll into the class now'}></SectionTitle>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
